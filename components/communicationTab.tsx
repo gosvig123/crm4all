9c4a1db82434f9850e7845ea384d3d9c528927b7
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-11-02 14:50:32 +0100

    feat, added sending email funactionality

--- components/communicationTab.tsx
+++ components/communicationTab.tsx
@@ -7,8 +7,14 @@ import {
 } from '@/components/ui/accordion';
 import { listEmails } from '@/lib/Gmail/readGmails';
 import { authorizeAndInitialize } from '@/lib/Gmail/GmailAuth';
+import { Button } from './ui/button';
 import { useState, useEffect, useRef } from 'react';
-
+import { EmailComposer } from './emailComposer';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from './ui/popover';
 type Email = {
   subject: string;
   from: string;
@@ -20,6 +26,20 @@ export function AccordionDemo() {
   const [gapi, setGapi] = useState<any>(null);
   const [emails, setEmails] = useState<Email[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [showEmailComposer, setShowEmailComposer] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const [recipient, setRecipient] = useState('');
+
+  const handleReplyButtonClick = (emailFrom: string) => {
+    setShowEmailComposer(true);
+    setRecipient(emailFrom);
+    setIsPopoverOpen(true);
+  };
+
+  const handlePopoverClose = () => {
+    setIsPopoverOpen(false);
+  };
 
   useEffect(() => {
     const loadGapi = async () => {
@@ -54,23 +74,35 @@ export function AccordionDemo() {
 
   const accordionContentRef = useRef<HTMLDivElement | null>(null);
 
-  return isLoading ? (
-    <div>Loading...</div>
-  ) : (
+  return (
     <Accordion
       type='single'
       collapsible
-      className='w-full h-[90vh] mt-5 overflow-y-auto'
+      className='w-full h-[90vh] mt-5 overflow-y-auto bg-white'
     >
       {emails.map((email, index) => (
         <AccordionItem value={`item-${index}`} key={index}>
-          <AccordionTrigger className='bg-white text-black border border-dotted p-2 flex flex-row overflow-hidden'>
+          <AccordionTrigger className='bg-white text-black border border-dotted p-2 flex flex-row gap-5 overflow-hidden'>
             <div className='font-bold text-left w-1/2 overflow-hidden flex-1 text-ellipsis whitespace-nowrap'>
               {email.subject}
             </div>
             <div className='text-sm'>
               Date: {formatDate(email.date)}
             </div>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={'outline'}
+                  className='border border-b-2 border-black bg-transparent text-black'
+                  onClick={() => handleReplyButtonClick(email.from)}
+                >
+                  Reply
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <EmailComposer gapi={gapi} from={recipient} />
+              </PopoverContent>
+            </Popover>
           </AccordionTrigger>
           <AccordionContent
             className='bg-white text-black max-h[50vh] overflow-auto'
---
+++ components/emailComposer.tsx
@@ -0,0 +1,62 @@
+// 'EmailComposer.tsx'
+
+'use client';
+import { Button } from './ui/button';
+import React, { useState } from 'react';
+import { sendEmail } from '@/lib/Gmail/SendEmail';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+
+type EmailComposerProps = {
+  gapi: any;
+  from: string;
+};
+
+export function EmailComposer({ gapi, from }: EmailComposerProps) {
+  const [to, setTo] = useState('');
+  const [subject, setSubject] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSendEmail = async () => {
+    if (!gapi || !to || !subject || !body) {
+      alert('Please fill out all fields');
+      return;
+    }
+
+    try {
+      await sendEmail(gapi, to, subject, body);
+      alert('Email sent successfully');
+      setTo('');
+      setSubject('');
+      setBody('');
+    } catch (error) {
+      alert('Failed to send email');
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className='flex flex-col gap-3 text-black  shadow-2xl'>
+      <Input
+        type='email'
+        placeholder='To'
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+      />
+      <Input
+        type='text'
+        placeholder='Subject'
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+      />
+      <Textarea
+        placeholder='Email body'
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+      <Button className='mt-5  w-full' onClick={handleSendEmail}>
+        Send
+      </Button>
+    </div>
+  );
+}
