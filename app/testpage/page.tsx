e6f32a746c0bbf38e00e1a365632d40c1d120c74
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-11-03 13:33:24 +0100

    fix, moved get auth token to run on server

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -3,40 +3,41 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 
+const getAccessToken = async (authorizationCode: string) => {
+  'use sever';
+  console.log('Fetching access token...', authorizationCode);
+  const response = await fetch(
+    'https://api.typeform.com/oauth/token',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        client_id: '5vujD8k4DQ97UMeHycGqiyZ5yKdiFj7Uyuo8iAB1o1wg',
+        client_secret: 'Gnr5fup1gauwC8GhHV9XyPDacGh5typQsUK6ZKp4AJUr',
+        redirect_uri: 'https://crm4all.vercel.app/api/typeform',
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    console.error(
+      'Failed to fetch access token:',
+      response.statusText
+    );
+    return;
+  }
+
+  const data = await response.json();
+  console.log('Access Token:', data.access_token);
+};
+
 const Page = () => {
   const [authCode, setAuthCode] = useState<string | null>(null);
 
-  const getAccessToken = async (authorizationCode: string) => {
-    console.log('Fetching access token...', authorizationCode);
-    const response = await fetch(
-      'https://api.typeform.com/oauth/token',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-          grant_type: 'authorization_code',
-          code: authorizationCode,
-          client_id: '5vujD8k4DQ97UMeHycGqiyZ5yKdiFj7Uyuo8iAB1o1wg',
-          client_secret:
-            'Gnr5fup1gauwC8GhHV9XyPDacGh5typQsUK6ZKp4AJUr',
-          redirect_uri: 'https://crm4all.vercel.app/testpage/',
-        }),
-      }
-    );
-
-    if (!response.ok) {
-      console.error(
-        'Failed to fetch access token:',
-        response.statusText
-      );
-      return;
-    }
-
-    const data = await response.json();
-    console.log('Access Token:', data.access_token);
-  };
   const handleClick = () => {
     window.location.href =
       'https://api.typeform.com/oauth/authorize?client_id=5vujD8k4DQ97UMeHycGqiyZ5yKdiFj7Uyuo8iAB1o1wg&redirect_uri=https://crm4all.vercel.app/testpage/&scope=forms:read+forms:write';
