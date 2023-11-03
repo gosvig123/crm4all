3d4ffe3cdb8d52e695c0c865868fae3796375fe8
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-11-03 12:11:22 +0100

    feat, added basic typeform auth to production.

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -2,17 +2,18 @@
 // filename: Page.tsx
 import dynamic from 'next/dynamic';
 import React from 'react';
-
-const GmailButtons = dynamic(
-  () => import('@/lib/Gmail/GmailButtons'),
-  { ssr: false } // This will only load GmailButtons on the client-side
-);
+import { Button } from '@/components/ui/button';
 
 const Page = () => {
+  const handleClick = async () => {
+    return fetch(
+      'https://api.typeform.com/oauth/authorize?client_id=5vujD8k4DQ97UMeHycGqiyZ5yKdiFj7Uyuo8iAB1o1wg&redirect_uri=https://crm4all.vercel.app/&scope=forms:read+forms:write'
+    );
+  };
   return (
     <div>
       <h1>Page</h1>
-      <GmailButtons />
+      <Button onClick={handleClick}>Button</Button>
     </div>
   );
 };
