bf7571ddca5a6d10a42b89266f31cb95a4830e6e
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-11-03 14:11:53 +0100

    moved call for oath to server

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,39 +1,7 @@
 'use client';
-// filename: Page.tsx
+import { getAccessToken } from '@/serverActions/getTypeformToken';
 import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
-
-const getAccessToken = async (authorizationCode: string) => {
-  'use sever';
-  console.log('Fetching access token...', authorizationCode);
-  const response = await fetch(
-    'https://api.typeform.com/oauth/token',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        grant_type: 'authorization_code',
-        code: authorizationCode,
-        client_id: '5vujD8k4DQ97UMeHycGqiyZ5yKdiFj7Uyuo8iAB1o1wg',
-        client_secret: 'Gnr5fup1gauwC8GhHV9XyPDacGh5typQsUK6ZKp4AJUr',
-        redirect_uri: 'https://crm4all.vercel.app/testpage/',
-      }),
-    }
-  );
-
-  if (!response.ok) {
-    console.error(
-      'Failed to fetch access token:',
-      response.statusText
-    );
-    return;
-  }
-
-  const data = await response.json();
-  console.log('Access Token:', data.access_token);
-};
 
 const Page = () => {
   const [authCode, setAuthCode] = useState<string | null>(null);
