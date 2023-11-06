0747378b6913148ba861fa301299351530f49a6b
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-11-06 15:51:34 +0100

    chore, addded suspense to kanban view

--- app/opportunities/page.tsx
+++ app/opportunities/page.tsx
@@ -4,6 +4,7 @@ import { Switch } from '@/components/ui/switch';
 
 import { editKanban } from '@/signals/signals';
 import KanbanBoard from '@/components/kanban';
+import { Suspense } from 'react';
 export default function Opportunities() {
   const handleToggle = () => {
     editKanban.value = !editKanban.value;
@@ -12,11 +13,13 @@ export default function Opportunities() {
   return (
     <main className='flex min-h-screen min-w-screen'>
       <Sidebar />
-      <KanbanBoard />
-      <div className='flex flex-col'>
-        Edit
-        <Switch onCheckedChange={handleToggle} />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <KanbanBoard />
+        <div className='flex flex-col'>
+          Edit
+          <Switch onCheckedChange={handleToggle} />
+        </div>
+      </Suspense>
     </main>
   );
 }
